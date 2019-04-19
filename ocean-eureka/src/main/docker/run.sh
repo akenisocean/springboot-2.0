83713f3a83b50720ae9100970a509174f1908072
akneisocean <jc164214878@163.com>
2019-04-19 18:25:46 +0800

    修改了Docekrfile内容

--- ocean-eureka/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting simple-service "
-echo "********************************************************"
-java -jar /usr/local/ocean-eureka/@project.build.finalName@.jar

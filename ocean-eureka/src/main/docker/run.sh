92a0744ca77af7cef1cdfa56b1d245fe069737ed
jc164 <jc164214878@163.com>
2019-04-19 16:08:25 +0800

    ocean-eureka添加了Dockerfile

---
+++ ocean-eureka/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting simple-service "
+echo "********************************************************"
+java -jar /usr/local/ocean-eureka/@project.build.finalName@.jar
